8ccc82c281fbe9830699d114d4cf25a7f8fd476c
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-16 17:40:31 +0100

    Aggiunti i box

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -1,31 +1,28 @@
 // Consegna:
 // Scrivi un programma che stampi i numeri da 1 a 100.
 
-// MILESTONE 1
-// Per i multipli di 3 stampi in console “Fizz” al posto del numero e per i multipli di 5 stampi in console Buzz.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi in console FizzBuzz.
+// MILESTONE 2
+// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 
 // Number - 1 to 100
 for( let i = 1; i <= 100; i++ ) {
 
     let fizzBuzz;
+    let squareContainer = document.getElementById("square-container");
     if( i % 3 === 0 && i % 5 === 0 ){
         fizzBuzz = 'FizzBuzz'
-        console.log(fizzBuzz);
+        squareContainer.innerHTML += `<div class="box">${fizzBuzz}</div>`
 
     } else if( i % 3 === 0 ) {
         fizzBuzz = 'Fizz'
-        console.log(fizzBuzz);
+        squareContainer.innerHTML += `<div class="box">${fizzBuzz}</div>`
 
     } else if( i % 5 === 0 ) {
         fizzBuzz = 'Buzz'
-        console.log(fizzBuzz);
+        squareContainer.innerHTML += `<div class="box">${fizzBuzz}</div>`
 
     } else {
-        console.log(i);
+        squareContainer.innerHTML += `<div class="box">${i}</div>`
     }
 
 }
-
-// MILESTONE 2
-// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
